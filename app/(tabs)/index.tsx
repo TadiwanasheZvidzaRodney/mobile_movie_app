e90247ba7d9be171d0be399fbc34b340740f93ae
tadiwanashezvidzarodney <tadiwarodneyzvidza@gmail.com>
2025-04-15 11:45:24 +0200

    react native routes, and navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
+import React from 'react'
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-primary font-bold">Hey there, Menace!</Text>
+      <Text className="text-md text-tertiary">Enjoy browsing our collection!</Text>
     </View>
   );
 }
